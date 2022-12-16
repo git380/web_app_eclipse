0dfffa2c2aa257e02aeefdb65de72bb3f6426dca
201010 <201010@WINDOWS-HFD5RFH.mr.hi-joho.ac.jp>
2022-12-16 11:36:43 +0900

    12/16

---
+++ 9-3/src/model/SiteEV.java
@@ -0,0 +1,37 @@
+package model;
+
+public class SiteEV {
+  private int like; // よいねの数
+  private int dislike; // よくないねの数
+
+  public SiteEV() {
+    like = 0;
+    dislike = 0;
+  }
+
+  public int getLike() {
+    return like;
+  }
+
+  public void setLike(int like) {
+    this.like = like;
+  }
+
+  public int getDislike() {
+    return dislike;
+  }
+
+  public void setDislike(int dislike) {
+    this.dislike = dislike;
+  }
+
+  public String getAns() {
+	  if(dislike < like) {
+		  return "よいね";
+	  } else if(dislike > like) {
+		  return "よくないね";
+	  } else {
+		  return "まぁまぁだね";
+	  }
+  }
+}
